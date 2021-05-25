ef85eceb45702cbe186d993e826a5f31bac8e19e
FareedKandalaft <fareed@spectrecoder.com>
2021-05-25 10:49:37 -0400

    complete

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// 
+const container = document.querySelector('.container');
+const unsplashURL = 'https://source.unsplash.com/random/';
+
+console.log('hello?');
+
+const rows = 10;
+
+for (let i = 0; i < (rows * 3); i++) {
+  const img = document.createElement('img');
+
+  const x = 300 + Math.floor(Math.random() * 10);
+  const y = 300 + Math.floor(Math.random() * 10);
+  let srcu = `${unsplashURL + x}x${y}`;
+  img.src = srcu;
+  container.appendChild(img);
+// console.log(srcu);    
+}
